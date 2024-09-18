49a10e5e8efa9c2b64a88957a00ba680b410f694
Akshat Yadav <71210501+Akshat1276@users.noreply.github.com>
2024-09-18 20:12:16 +0530

    Create while_loop.cpp

---
+++ while_loop.cpp
@@ -0,0 +1,16 @@
+class Geeks {
+static void printTable (int n) 
+{
+            //int n=1;      
+            int multiplier=10;
+            while(multiplier>0)
+            {
+               int m= n*multiplier;
+               System.out.print(m+" ");
+               multiplier--;
+               
+            }
+       System.out.println();
+}
+ 
+}
