7f7a1c17d59766f8975b61a6b29b5130f7cf3c78
Akshat Yadav <71210501+Akshat1276@users.noreply.github.com>
2024-09-22 22:31:28 +0530

    Add files via upload

---
+++ example.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+struct boom{
+  int number;
+  char alpha;
+};
+int main(){
+  struct boom shit;
+  shit.number = 12;
+  shit.alpha = 'a';
+  printf("%d\n", shit.number);
+  printf("%c\n", shit.alpha);
+  return 0;
+}
