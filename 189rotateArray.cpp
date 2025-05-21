f6df95d3a7a48047a4e3edaefd144a1790c91f7c
Akshat Yadav <71210501+Akshat1276@users.noreply.github.com>
2025-05-21 21:54:43 +0530

    Create 189rotateArray.cpp

---
+++ 189rotateArray.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    void rotate(vector<int>& nums, int k) {
+        int n = nums.size();
+        vector<int> s(nums.begin(), nums.end());
+        for(int i=0; i<n; i++){
+            s[(i+k)%n] = nums[i];
+        }
+        nums.assign(s.begin(), s.end());
+    }
+};
