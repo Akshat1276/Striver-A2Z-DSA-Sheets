5574c7dadf6f9817747de7cb1261093df6a8b21d
Akshat Yadav <71210501+Akshat1276@users.noreply.github.com>
2025-06-06 19:15:08 +0530

    Create fibbonacci.cpp

---
+++ fibbonacci.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int fib(int n) {
+        if (n == 0) return 0;
+        if (n == 1) return 1;
+        vector<int> fibb(n + 1);
+        fibb[0] = 0;
+        fibb[1] = 1;
+        for (int i = 2; i <= n; i++) {
+            fibb[i] = fibb[i - 1] + fibb[i - 2];
+        }
+        return fibb[n];
+    }
+};
